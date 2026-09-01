0d90c15ce93c2cec4877ccae943fbae0c7ac7e35
tonyyang889 <115537472+tonyyang889@users.noreply.github.com>
2026-09-01 12:43:43 +0800

    Update 闪雷.js

--- json1/闪雷.js
+++ json1/闪雷.js
@@ -1,7 +1,7 @@
 var rule = {
     title:'闪雷影视',
     编码:'gb2312',
-    host:'http://223.100.63.144:82',
+    host:'http://114.100.48.52:18008',
     url:'/www/List.asp?classid=fyclass&searchword=&page=fypage',
     filterable:0,//是否启用分类筛选,
     class_name:'电影&电视剧&综艺&动漫&音乐',
